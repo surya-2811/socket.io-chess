0e6780e52012cb31fcbbd72cf424d48a169aa447
surya-2811 <suryau@quaticstech.in>
2022-03-18 20:16:46 +0530

    text changes fixed

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-var path         = require('path')
-  , http         = require('http')
-  , express      = require('express')
-  , socket       = require('socket.io')
-  , httpRoutes   = require('./routes/http')
-  , socketRoutes = require('./routes/socket')
-  , GameStore    = require('./lib/GameStore');
+var path = require("path"),
+  http = require("http"),
+  express = require("express"),
+  socket = require("socket.io"),
+  httpRoutes = require("./routes/http"),
+  socketRoutes = require("./routes/socket"),
+  GameStore = require("./lib/GameStore");
 
-var app    = express()
-  , server = http.createServer(app)
-  , io     = socket.listen(server);
+var app = express(),
+  server = http.createServer(app),
+  io = socket.listen(server);
 
 var DB = new GameStore();
 
-var cookieParser = express.cookieParser('I wish you were an oatmeal cookie')
-  , sessionStore = new express.session.MemoryStore();
+var cookieParser = express.cookieParser("I wish you were an oatmeal cookie"),
+  sessionStore = new express.session.MemoryStore();
 
 // Settings
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set("port", process.env.PORT || 4000);
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
 
 // Middleware
-app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
-app.use(express.logger('dev'));
+app.use(express.favicon(__dirname + "/public/img/favicon.ico"));
+app.use(express.logger("dev"));
 app.use(express.bodyParser());
 app.use(cookieParser);
 app.use(express.session({ store: sessionStore }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(app.router);
-if ('development' == app.get('env')) {
+if ("development" == app.get("env")) {
   app.use(express.errorHandler());
 }
 
@@ -37,15 +37,18 @@ if ('development' == app.get('env')) {
  * This requires re-purposing Express's cookieParser middleware in order to expose
  * the session info to Socket.IO
  */
-io.set('authorization', function (handshakeData, callback) {
-  cookieParser(handshakeData, {}, function(err) {
+io.set("authorization", function (handshakeData, callback) {
+  cookieParser(handshakeData, {}, function (err) {
     if (err) return callback(err);
-    sessionStore.load(handshakeData.signedCookies['connect.sid'], function(err, session) {
-      if (err) return callback(err);
-      handshakeData.session = session;
-      var authorized = (handshakeData.session) ? true : false;
-      callback(null, authorized);
-    });
+    sessionStore.load(
+      handshakeData.signedCookies["connect.sid"],
+      function (err, session) {
+        if (err) return callback(err);
+        handshakeData.session = session;
+        var authorized = handshakeData.session ? true : false;
+        callback(null, authorized);
+      }
+    );
   });
 });
 
@@ -54,6 +57,6 @@ httpRoutes.attach(app, DB);
 socketRoutes.attach(io, DB);
 
 // And away we go
-server.listen(app.get('port'), function(){
-  console.log('Socket.IO Chess is listening on port ' + app.get('port'));
+server.listen(app.get("port"), function () {
+  console.log("Socket.IO Chess is listening on port " + app.get("port"));
 });
